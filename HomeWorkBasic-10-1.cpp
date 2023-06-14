e8bbf4797e0e0c09309b32787e03d973bec5a4ae
Ivan <kosarevsky2000@yandex.ru>
2023-06-14 22:48:54 +0300

    initial commit

---
+++ HomeWorkBasic-10-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Введите имя: ";
+	std::string name{};
+	std::cin >> name;
+	std::cout << "Привет, " << name << "!";
+}
